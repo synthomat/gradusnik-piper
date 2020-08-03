412ac3f555e56e9912a6c7349ddb7e8067981bce
Anton Zering <synth@lostprofile.de>
2020-08-03 07:15:52 +0200

    refactoring parameters

--- piper.go
+++ piper.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	influxdb2 "github.com/influxdata/influxdb-client-go"
+	"github.com/influxdata/influxdb-client-go/api"
 	log "github.com/sirupsen/logrus"
 	"os"
 	"time"
@@ -12,48 +14,119 @@ import (
 
 type Aqara struct {
 	Temperature float32 `json:"temperature"`
-	Humidity float32 `json:"humidity"`
-	Pressure float32 `json:"pressure"`
+	Humidity    float32 `json:"humidity"`
+	Pressure    float32 `json:"pressure"`
+}
+
+type MeasureData map[string]interface{}
+
+type Storage interface {
+	LogData(data Aqara)
+}
+
+type InfluxDB struct {
+	client influxdb2.Client
+	writer api.WriteAPI
+}
+
+func (idb InfluxDB) LogData(data Aqara) {
+	fmt.Println("Logging ", data)
+
+	tags := map[string]string{"unit": "temperature"}
+	values := map[string]interface{}{
+		"temp":  data.Temperature,
+		"hum":   data.Humidity,
+		"press": data.Pressure,
+	}
+
+	p := influxdb2.NewPoint(
+		"sensor",
+		tags,
+		values,
+		time.Now())
+
+	idb.writer.WritePoint(p)
+}
+
+type InfluxDBConfig struct {
+	Url   string
+	Token string
+}
+
+func NewInfluxDBTarget(config InfluxDBConfig) (influx InfluxDB) {
+	influx = InfluxDB{}
+	influx.client = influxdb2.NewClient(config.Url, config.Token)
+	influx.writer = influx.client.WriteAPI("", "sensors")
+
+	return
+}
+
+type MQTTConfig struct {
+	Topic  string
+	Broker string
+	User   string
+	Pass   string
+}
+
+type Config struct {
+	mqtt     MQTTConfig
+	influxdb InfluxDBConfig
+}
+
+func GetConfig() Config {
+	config := Config{}
+
+	config.mqtt = MQTTConfig{
+		Topic:  *flag.String("mqtt-topic", "sensors", "The topic name to/from which to publish/subscribe"),
+		Broker: *flag.String("mqtt-broker", "tcp://192.168.1.105:1883", "The broker URI. ex: tcp://10.10.1.1:1883"),
+		User:   *flag.String("mqtt-user", "", "The User (optional)"),
+		Pass:   *flag.String("mqtt-password", "", "The password (optional)"),
+	}
+
+	config.influxdb = InfluxDBConfig{
+		Url:   *flag.String("influx-url", "http://192.168.1.105:8086", "InfluxDB Server URL"),
+		Token: *flag.String("influx-token", "", "InfluxDB Token (optional)"),
+	}
+
+	flag.Parse()
+
+	return config
 }
 
 func main() {
-	brokerUrl := "tcp://192.168.1.105:1883"
+	config := GetConfig()
+	log.Info(config)
+
+	target := NewInfluxDBTarget(config.influxdb)
+	brokerUrl := config.mqtt.Broker
+
 	log.Info("Connecting to broker ", brokerUrl)
-	opts := mqtt.NewClientOptions().AddBroker(brokerUrl)
-	opts.SetKeepAlive(2 * time.Second)
-	opts.SetPingTimeout(1 * time.Second)
-
-	client := influxdb2.NewClient("http://192.168.1.105:8086", "")
-	// user blocking write client for writes to desired bucket
-	writeAPI := client.WriteAPI("", "sensors")
-
+	opts := mqtt.NewClientOptions().
+		AddBroker(brokerUrl).
+		SetUsername(config.mqtt.User).
+		SetPassword(config.mqtt.Pass).
+		SetKeepAlive(2 * time.Second).
+		SetPingTimeout(1 * time.Second)
 
 	c := mqtt.NewClient(opts)
 	if token := c.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
 
-	if token := c.Subscribe("zigbee2mqtt/+", 0, func(client mqtt.Client, message mqtt.Message) {
-		fmt.Println(message.Topic())
+	messageHandler := func(client mqtt.Client, message mqtt.Message) {
 		var data Aqara
 		if err := json.Unmarshal(message.Payload(), &data); err != nil {
-			panic(err)
+			log.Error("Cannot parse JSON", err)
+			return
 		}
 		fmt.Println(data)
-		p := influxdb2.NewPointWithMeasurement("sensor").
-			AddTag("unit", "temperature").
-			AddField("temp", data.Temperature).
-			AddField("hum", data.Humidity).
-			AddField("press", data.Pressure).
-			SetTime(time.Now())
-		writeAPI.WritePoint(p)
+		target.LogData(data)
+	}
 
-	}); token.Wait() && token.Error() != nil {
+	if token := c.Subscribe("zigbee2mqtt/+", 0, messageHandler); token.Wait() && token.Error() != nil {
 		fmt.Println(token.Error())
 		os.Exit(1)
 	}
-
-
 
 	select {}
 }
